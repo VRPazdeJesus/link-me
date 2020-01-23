29c3b85dc2254340f7a024eec072c01860261372
Víctor Romário <victor.romario.seriado@gmail.com>
2020-01-23 18:50:23 -0300

    Adicionando font

---
+++ js/init.js
@@ -0,0 +1,23 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDG_zcHkBgsKHHAaHxEqU3NZ48xZ6gJ3_M",
+    authDomain: "link-me-d34a1.firebaseapp.com",
+    databaseURL: "https://link-me-d34a1.firebaseio.com",
+    projectId: "link-me-d34a1",
+    storageBucket: "link-me-d34a1.appspot.com",
+    messagingSenderId: "953083618137",
+    appId: "1:953083618137:web:166e28c4c5474ab19d6248"
+};
+firebase.initializeApp(firebaseConfig);
+
+// Link me 2
+// var firebaseConfig = {
+//     apiKey: "AIzaSyDoJbV2iZ_cFs4wwzoI93HmTlCGRj2a98s",
+//     authDomain: "linkme2-91db8.firebaseapp.com",
+//     databaseURL: "https://linkme2-91db8.firebaseio.com",
+//     projectId: "linkme2-91db8",
+//     storageBucket: "linkme2-91db8.appspot.com",
+//     messagingSenderId: "754919246352",
+//     appId: "1:754919246352:web:9c093548888696dbad5895"
+// };
+// Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
--- js/linkme.js
+++ js/linkme.js
@@ -1,31 +1,13 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyDG_zcHkBgsKHHAaHxEqU3NZ48xZ6gJ3_M",
-    authDomain: "link-me-d34a1.firebaseapp.com",
-    databaseURL: "https://link-me-d34a1.firebaseio.com",
-    projectId: "link-me-d34a1",
-    storageBucket: "link-me-d34a1.appspot.com",
-    messagingSenderId: "953083618137",
-    appId: "1:953083618137:web:166e28c4c5474ab19d6248"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-
-function myFunction() {
-    var emailTxt = document.getElementById('inputEmail').value;
-    var textArea = document.getElementById('inputTextarea').value;
-    var database = firebase.database();
+function enviar() {
+    var emailValue = document.getElementById('emailTxt').value;
+    var areaValue = document.getElementById('areaTxt').value;
     var timestamp = new Date().getTime();
-    
-    database.ref(timestamp).set({
-        email: emailTxt,
-        text: textArea
+    // A variavel database vai recer as funções de acesso ao banco de dados
+    var database = firebase.database();
+    database.ref('usuario').set({
+        email: emailValue,
+        mensagem: areaValue
     });
-
-    $('#exampleModal').modal('toggle');
-
-    document.getElementById('inputEmail').value = '';
-    document.getElementById('inputTextarea').value = '';
-
-    alert("Mensagem enviada com sucesso!");
+    
+    
 }
